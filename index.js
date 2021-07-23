e5ee5c4395e385e0ac439d78893125f508d7e9e7
Anshul072 <76039686+Anshul072@users.noreply.github.com>
2021-07-23 17:28:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,6 +28,52 @@ app.post('/newapi/project',async (req,res)=>{
 }
 
 });
+app.get('/newapi/project', async (req,res)=>{
+    try{
+        const products = await Product.find();
+        return res.status(200).send(products);
+    }catch(e){
+        return res.status(500).send(e)
+    }
+});
+app.get('/newapi/project/:id', async (req,res)=>{
+    const _id=req.params.id;
+    try{
+        const product =await Product.findById(_id);
+        return res.status(200).send(product);
+    }catch(e){
+        return res.status(500).send(e)
+    }
+});
+app.delete('/newapi/project/:id', async (req,res)=>{
+    const _id=req.params.id;
+    try{
+        const product =await Product.findByIdAndDelete(_id);
+        if(product){
+            res.status(400).send("Product Deleted Successfully");
+        }
+        else{
+            res.send("No Such Product");
+        }
+    }catch(e){
+        return res.status(500).send(e)
+    }
+});
+app.patch('/newapi/project/:id', async(req,res)=>{
+    const _id = req.params.id;
+    try{
+        const product =await Product.findByIdAndUpdate(_id,req.body);
+        if(product){
+            const productUp = await Product.findById(_id);
+            res.status(200).send(productUp);
+        }
+        else{
+            res.status(400).send("Update failed")
+        }
+    }catch(e){
+        return res.status(500).send(e)
+    }
+});
 
 
 app.listen(3000,()=>{console.log("Listening on port 3000")})
